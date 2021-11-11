df296313d9a7cfe87d5bdea502d2b2a11fad188a
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-11 11:50:12 +0200

    Задание 2 решение

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// находим галлерею
+const galleryEl = document.querySelector("ul.gallery");
+console.log(galleryEl);
+
+// создаем в памяти список изображений с атрибутами, полученные из galleryItems
+const listEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+//   Вводим галлерею на страницу
+galleryEl.insertAdjacentHTML("afterbegin", listEl);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
